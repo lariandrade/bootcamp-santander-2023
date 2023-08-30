c875698770659a1d175d5eb07d0eafd712bb7bc8
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-30 19:43:11 -0300

    adicionado descricao do exercicio

--- Java/ExerciciosCollections/Set/OperacoesBasicas/Convidados/Main.java
+++ Java/ExerciciosCollections/Set/OperacoesBasicas/Convidados/Main.java
@@ -1,5 +1,14 @@
 package Java.ExerciciosCollections.Set.OperacoesBasicas.Convidados;
 
+/*
+ * Crie uma classe chamada "ConjuntoConvidados" que possui um conjunto de objetos do tipo "Convidado" como atributo. 
+ * Cada convidado possui atributos como nome e código do convite. 
+ * Implemente os seguintes métodos:
+ * - adicionarConvidado(String nome, int codigoConvite): Adiciona um convidado ao conjunto.
+ * - removerConvidadoPorCodigoConvite(String codigoConvite): Remove um convidado do conjunto com base no código do convite.
+ * - contarConvidados(): Conta o número total de convidados no Set.
+ * - exibirConvidados(): Exibe todos os convidados do conjunto.
+ */
 public class Main {
     public static void main(String[] args) {
         ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
