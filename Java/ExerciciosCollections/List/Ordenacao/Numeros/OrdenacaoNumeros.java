f44b597e1a41657d7b5c9122031bd3d8f6605d52
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-30 14:29:59 -0300

    exercicio "Ordenação de Números" resolvido

---
+++ Java/ExerciciosCollections/List/Ordenacao/Numeros/OrdenacaoNumeros.java
@@ -0,0 +1,38 @@
+package Java.ExerciciosCollections.List.Ordenacao.Numeros;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    private List<Integer> numeros;
+
+    public OrdenacaoNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+        numeros.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numeros);
+        if (!numeros.isEmpty()) {
+            Collections.sort(numerosAscendente);
+        }
+        return numerosAscendente;
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosAscendente = new ArrayList<>(this.numeros);
+        if (!numeros.isEmpty()) {
+            numerosAscendente.sort(Collections.reverseOrder());
+        }
+        return numerosAscendente;
+    }
+
+    public void exibirNumeros() {
+        System.out.println(this.numeros);
+    }
+
+}
