6ccd10942fd51cb4f4b8f433e92587ffbd002da8
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-30 10:22:15 -0300

    testando metodos

---
+++ Java/ExerciciosCollections/List/Pesquisa/Livros/Main.java
@@ -0,0 +1,40 @@
+package Java.ExerciciosCollections.List.Pesquisa.Livros;
+
+/*
+ * Crie uma classe chamada "CatalogoLivros" que possui uma lista de objetos do tipo "Livro" como atributo. Cada livro possui atributos como título, autor e ano de publicação. 
+ * Implemente os seguintes métodos:
+ * - adicionarLivro(String titulo, String autor, int anoPublicacao): Adiciona um livro ao catálogo.
+ * - pesquisarPorAutor(String autor): Pesquisa livros por autor e retorna uma lista com os livros encontrados.
+ * - pesquisarPorIntervaloAnos(int anoInicial, int anoFinal): Pesquisa livros publicados em um determinado intervalo de anos e retorna uma lista com os livros encontrados.
+ * - pesquisarPorTitulo(String titulo): Pesquisa livros por título e retorna o primeiro livro encontrado.
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        // Adicionando livros ao catálogo
+        catalogoLivros.adicionarLivro("Microsserviços Prontos Para a Produção", "Susan J. Fowler", 2017);
+        catalogoLivros.adicionarLivro("Java Guia do Programador", "Peter Jandl Junior", 2021);
+        catalogoLivros.adicionarLivro("Código Limpo", "Robert C. Martin", 2009);
+        catalogoLivros.adicionarLivro("O Codificador Limpo", "Robert C. Martin", 2012);
+
+        // Exibindo livros pelo mesmo autor
+        System.out.println(catalogoLivros.pesquisarPorAutor("Robert C. Martin"));
+
+        // Exibindo livros pelo mesmo autor (caso em que não há livros de um autor específico)
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor Inexistente"));
+
+        // Exibindo livros dentro de um intervalo de anos
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2010, 2022));
+
+        // Exibindo livros dentro de um intervalo de anos (caso em que não há livros no intervalo)
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2025, 2030));
+
+        // Exibindo livros por título
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Java Guia do Programador"));
+
+        // Exibindo livros por título (caso em que não há livros com o título especificado)
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Título Inexistente"));
+    }
+}
