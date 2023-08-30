6784821c8d8ddf47af0b431190504fc9b4241d82
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-08-30 19:55:15 -0300

    exercicio "Conjunto de Palavras Unicas" resolvido

---
+++ Java/ExerciciosCollections/Set/OperacoesBasicas/Palavras/Main.java
@@ -0,0 +1,42 @@
+package Java.ExerciciosCollections.Set.OperacoesBasicas.Palavras;
+
+/*
+ * Crie uma classe chamada "ConjuntoPalavrasUnicas" que possui um conjunto de palavras únicas como atributo. 
+ * Implemente os seguintes métodos:
+ * - adicionarPalavra(String palavra): Adiciona uma palavra ao conjunto.
+ * -  removerPalavra(String palavra): Remove uma palavra do conjunto.
+ * -  verificarPalavra(String palavra): Verifica se uma palavra está presente no conjunto.
+ * -  exibirPalavrasUnicas(): Exibe todas as palavras únicas do conjunto.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Criando uma instância da classe ConjuntoPalavrasUnicas
+        ConjuntoPalavrasUnicas conjuntoLinguagens = new ConjuntoPalavrasUnicas();
+
+        // Adicionando linguagens únicas ao conjunto
+        conjuntoLinguagens.adicionarPalavra("Java");
+        conjuntoLinguagens.adicionarPalavra("Python");
+        conjuntoLinguagens.adicionarPalavra("Java");
+        conjuntoLinguagens.adicionarPalavra("Python");
+        conjuntoLinguagens.adicionarPalavra("C++");
+        conjuntoLinguagens.adicionarPalavra("Ruby");
+
+        // Exibindo as linguagens únicas no conjunto
+        conjuntoLinguagens.exibirPalavrasUnicas();
+
+        // Removendo uma linguagem do conjunto
+        conjuntoLinguagens.removerPalavra("Python");
+        conjuntoLinguagens.exibirPalavrasUnicas();
+
+        // Removendo uma linguagem inexistente
+        conjuntoLinguagens.removerPalavra("Swift");
+
+        // Verificando se uma linguagem está no conjunto
+        System.out.println("A linguagem 'Javas' está no conjunto? " + conjuntoLinguagens.verificarPalavra("Javas"));
+        System.out.println("A linguagem 'Python' está no conjunto? " + conjuntoLinguagens.verificarPalavra("Python"));
+
+        // Exibindo as linguagens únicas atualizadas no conjunto
+        conjuntoLinguagens.exibirPalavrasUnicas();
+
+    }
+}
